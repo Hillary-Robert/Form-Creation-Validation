850aa3230529b475c8a7f0ec9d961af20fb623dd
Hillary Robert <roberthillary30@gmail.com>
2025-01-24 19:20:24 +0200

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
-  form.addEventListener("submit", (event) => {
+  form.addEventListener("submit", function(event){
       event.preventDefault();
 
       let username = document.getElementById("username").value.trim();
